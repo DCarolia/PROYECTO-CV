a7c4d61f505593c359c91f29212e3af52806359c
DCarolia <carolina.dominguez2217@gmail.com>
2023-08-31 18:07:45 -0300

    VERSION1

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function cambiar(){
+    document.getElementById("full").innerHTML="FULL STACK DEVELOPER JUNIOR";
+}
+
+function bannerDos(){
+    document.getElementById("banner").src="./assets/banner2.jpg";
+}
